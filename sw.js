eabc5bdd1a679878245487e8f11a02d5f09781f4
RDF-74 <renbike74@yahoo.ne.jp>
2026-08-09 00:43:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='detailing-v2.5.2';
+const CACHE='detailing-v2.5.3';
 const STATIC=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
