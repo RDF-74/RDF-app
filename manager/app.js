cb16dd7f769a9eacacac535c08101b5a5e8114d4
RDF-74 <renbike74@yahoo.ne.jp>
2026-09-09 21:59:52 +0900

    Hide chemical section for rinse step

--- manager/app.js
+++ manager/app.js
@@ -2344,7 +2344,7 @@ async function renderServiceTimer(recordId) {
     confirmationProposals = proposalRows || [];
     confirmationMarkup = serviceConfirmationMarkup(confirmationProposals, steps);
   }
-  const chemicalMarkup = isPreCheck ? "" : serviceChemicalUsageMarkup(activeStep, chemicalUsages, chemicals, chemicalStandards);
+  const chemicalMarkup = isPreCheck || activeStep.step_key === "step_600" ? "" : serviceChemicalUsageMarkup(activeStep, chemicalUsages, chemicals, chemicalStandards);
   const previousStepMarkup = previousStep
     ? '<button class="text-button" type="button" id="previousServiceStepButton">← 前の工程へ戻る</button>'
     : "";
